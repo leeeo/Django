97a03a909c91805883d2a2420eded6617a1315ef
Eojin Lee <wihappi1646@gamil.com>
2019-09-09 20:44:01 +0900

    Django 설정-Model 구성

--- eojin_django/eojinuser/models.py
+++ eojin_django/eojinuser/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
+class Eojinuser(models.Model):
+    email = models.EmailField(verbose_name='이메일')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    register_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta:
+        db_table = 'eojin_user'
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자'
--- eojin_django/order/models.py
+++ eojin_django/order/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Order(models.Model):
+    eojinuser = models.ForeignKey(
+        'eojinuser.Eojinuser', on_delete=models.CASCADE, verbose_name='사용자')
+    product = models.ForeignKey(
+        'product.Product', on_delete=models.CASCADE, verbose_name='상품')
+    quantity = models.IntegerField(verbose_name='수량')
+    register_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta:
+        db_table = 'eojin_order'
+        verbose_name = '주문'
+        verbose_name_plural = '주문'
