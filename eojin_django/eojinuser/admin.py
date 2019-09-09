cf4756f740f1c214c7916a8d1b7520810f0591e9
Eojin Lee <wihappi1646@gamil.com>
2019-09-09 21:17:05 +0900

    Django 설정-Admin 구성

--- eojin_django/eojinuser/admin.py
+++ eojin_django/eojinuser/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Eojinuser
 
 # Register your models here.
+
+
+class EojinuserAdmin(admin.ModelAdmin):
+    list_display = ('email', )
+
+
+admin.site.register(Eojinuser, EojinuserAdmin)
