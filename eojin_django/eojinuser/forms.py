86b9d581c62a7566137f38d8c3f1b73d3d16f31f
Eojin Lee <wihappi1646@gamil.com>
2019-09-12 00:06:33 +0900

    Class-based view 기본-로그인 만들기

--- eojin_django/eojinuser/forms.py
+++ eojin_django/eojinuser/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.db import models
+from django.contrib.auth.hashers import check_password, make_password
 from .models import Eojinuser
 
 class RegisterForm(forms.Form):
@@ -6,7 +8,7 @@ class RegisterForm(forms.Form):
         error_messages={
             'required': '이메일을 입력해주세요.'
         },
-        max_length=64, label='비밀번호'
+        max_length=64, label='이메일'
     )
     password = forms.CharField(
         error_messages={
@@ -23,6 +25,7 @@ class RegisterForm(forms.Form):
 
     def clean(self):
         cleaned_data = super().clean()
+        email = cleaned_data.get('email')
         password = cleaned_data.get('password')
         re_password = cleaned_data.get('re_password')
 
@@ -33,6 +36,39 @@ class RegisterForm(forms.Form):
             else:
                 eojinuser = Eojinuser(
                     email=email,
-                    password=password
+                    password=make_password(password)
                 )
                 eojinuser.save()
+
+class LoginForm(forms.Form):
+    objects = models.Manager()
+    email = forms.EmailField(
+        error_messages={
+            'required': '이메일을 입력해주세요.'
+        },
+        max_length=64, label='이메일'
+    )
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
+        widget=forms.PasswordInput, label='비밀번호'
+    )
+    
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                eojinuser = Eojinuser.objects.get(email=email)
+            except Eojinuser.objects.DoesNotExist:
+                self.add_error('email', '아이디가 없습니다')
+                return
+
+            if not check_password(password, eojinuser.password):
+                self.add_error('password', '비밀번호가 틀렸습니다')
+            else:
+                self.email = eojinuser.email
