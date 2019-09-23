fe67c5b382015a37f99f8f965eba553a439874ea
Eojin Lee <wihappi1646@gamil.com>
2019-09-23 22:15:45 +0900

    Class-based view 기본-상품 주문하기

--- eojin_django/order/forms.py
+++ eojin_django/order/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Order
+from product.models import Product
+from eojinuser.models import Eojinuser
 
 class RegisterForm(forms.Form):
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+
     quantity = forms.IntegerField(
         error_messages={
             'required': '수량을 입력해주세요.'
@@ -14,10 +20,20 @@ class RegisterForm(forms.Form):
         }, label='상품설명', widget=forms.HiddenInput
     )
     
-
     def clean(self):
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
-
+        eojinuser = self.request.session.get('user')
            
+        if quantity and product and eojinuser:
+            order = Order(
+                quantity = quantity,
+                product = Product.objects.get(pk=product),
+                eojinuser=Eojinuser.objects.get(email=eojinuser)
+            )
+            order.save()
+        else:
+            self.product = product
+            self.add_error('quantity', '값이 없습니다')
+            self.add_error('product', '값이 없습니다')
